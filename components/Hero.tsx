d11d3fee4856daf57af04106b75a46ea3588bca6
vigneshsalesforce <e.vignesh1999@gmail.com>
2025-06-01 23:02:41 +0530

    fix:image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,7 +20,7 @@ export const Hero = () => {
       {/* Full Cover Hero Image */}
       <div className="absolute inset-0">
         <img 
-          src="https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2064&q=80"
+          src="/car_hero.jpg"
           alt="Luxury vehicle"
           className="w-full h-full object-cover"
         />
